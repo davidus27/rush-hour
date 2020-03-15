976ef717d68e26c148df52d02f6335177f8206da
davidus27 <davidisbest27@gmail.com>
2020-03-15 02:00:27 +0100

    adding traffic class for easier process in future

--- zadanie.py
+++ zadanie.py
@@ -12,6 +12,7 @@ class Color(Enum):
     blue = 6
     green = 7
 
+
 class Size(Enum):
     small = 2
     medium = 3
@@ -22,26 +23,40 @@ class Direction(Enum):
     vertical = "v"
 
 
-
 class Vehicle(object):
-    def __init__(self, color, starting_position, size, direction):
+    def __init__(self, color = Color.none, starting_position = (0,0), size = Size.medium, direction = Direction.vertical):
         self.color = color
-        self.starting_position = starting_postion
+        self.starting_position = starting_position
         self.size = size
         self.direction = direction
-        self.postion = self.get_position()
 
-    def get_postion(self):
-            
+    @property
+    def position(self):
+        positions = []
+        for i in range(self.size.value):
+            y = self.starting_position[0]
+            x = self.starting_position[1]
+            if x < 0 or y < 0 or x + i > 6 or y +i > 6:
+                return None
+            if self.direction is Direction.vertical:
+                positions.append((x,y+i))
+            if self.direction is Direction.horizontal:
+                positions.append((x+i,y))
+        return positions 
+
 
 class Traffic(object):
     def __init__(self):
         self.road = zeros((6,6))
 
     def add_vehicle(self, vehicle):
-        pass         
-
-
+        if vehicle.position is None:
+            return None
+        for position in vehicle.position:
+            y = position[0]
+            x = position[1]
+            self.road[x][y] = vehicle.color.value
+        return self
 
 
 
