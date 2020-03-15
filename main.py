64f044d1b9da4741f07e4bdccffbd36eb297877c
davidus27 <davidisbest27@gmail.com>
2020-03-15 02:01:36 +0100

    adding test use cases of traffic system

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import zadanie
 
-def main():
-    print("skuska")
+def generate_cars():
+    cars = [zadanie.Vehicle(color = zadanie.Color(i), starting_position = [i,0]) for i in range(6)]
+    
+    for index,car in enumerate(cars):
+        car.size = zadanie.Size.medium
+        car.direction = zadanie.Direction.horizontal
+    return cars
 
+def main():
+    
+    cars = generate_cars()
+    traffic = zadanie.Traffic()
+    for car in cars:
+            traffic.add_vehicle(car)
+    print(traffic.road)
+
+
+        
 if __name__ == "__main__":
     main()
 
