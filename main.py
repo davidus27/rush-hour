0e17a618d6fba21d701b7db1928a7633eafbf21e
davidus27 <davidisbest27@gmail.com>
2020-03-16 16:02:58 +0100

    adding working with file for easier config settings

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import zadanie
 
+
 def generate_cars():
     cars = [zadanie.Vehicle(color = zadanie.Color(i+1), starting_position = [i,0]) for i in range(7)]
     
@@ -8,29 +9,39 @@ def generate_cars():
         car.direction = zadanie.Direction.horizontal
     return cars
 
-
-
 def read_file(file_name):
     try:
-        with open(file_name):
-            data = f.read()
-        f.close()
-        return data
+        f = open(file_name, "r")
+        cars = []
+        for line in f:
+            cars.append(line.split())
+        return cars
     except:
         print("Problem with file.")
         return None
+
+def evaluate_color(str_color):
+    for c in zadanie.Color:
+        if c.name == str_color: 
+            return c
+    return None
 
 def create_vehicles(data):
     if data is None:
         print("Error: No data available.")
         return None
-    for i in data:
-        print(i)
 
-
+    cars = []
+    print(data)
+    for i in data: 
+        color = evaluate_color(i[0])
+        size = zadanie.Size(int(i[1]))
+        cordinates = ( (int(i[2]), int(i[3])) )
+        direction = zadanie.Direction(i[4])
+        cars.append(zadanie.Vehicle(color, size, cordinates, direction))
+    return cars
     
 def main():
-    
     cars = generate_cars()
     traffic = zadanie.Traffic()
     for car in cars:
@@ -38,12 +49,9 @@ def main():
     print(traffic.road)
     
     #new thingy
-    #create_vehicles(read_file("file.txt"))
-    with open("file.txt"):
-        data = f.read()
-    print(data)
-
-        
+    cars = create_vehicles(read_file("testing_config.txt"))
+    
+       
 if __name__ == "__main__":
     main()
 
