7290e0210b275b8772f4e162f9cba5bc10fe5ec5
davidus27 <davidisbest27@gmail.com>
2020-03-18 22:36:04 +0100

    Few changes

--- src/main.py
+++ src/main.py
@@ -3,14 +3,13 @@ import zadanie
 
    
 def main():
-    cars = generating.create_vehicles(generating.read_file("../configs/testing_config.txt"))
+    cars = generating.create_vehicles(generating.read_file("configs/testing_config.txt"))
     print(cars)
-
+    
     traffic = zadanie.Traffic()
     for car in cars:
             traffic.add_vehicle(car)
     print(traffic.road)
-    
        
 if __name__ == "__main__":
     main()
