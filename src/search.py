3b09498fa933d3e8963325408772e28c4081ad47
davidus27 <davidisbest27@gmail.com>
2020-03-25 23:57:00 +0100

    omg

--- src/search.py
+++ src/search.py
@@ -4,6 +4,7 @@ class IterativeDeepening(object):
     def __init__(self, traffic, vehicles):
         self.traffic = traffic
         self.path = [] #storing actual path from root
+        self.visited = []
         
     def iddfs(self):
         #escaped = control_unit.is_goal(self.vehicles[red_position])
@@ -11,18 +12,13 @@ class IterativeDeepening(object):
         while True:
             found, remaining = self.dls(self.traffic, depth)
             if found:
-                return found
+                return found.road
             elif not remaining:
                 return None
             depth += 1
             print(depth)
-            #just testing
-            if depth > 10:
-                return False
+            self.visited = []
 
-    def dfs_iterative(self, depth):
-        pass
-   
     def _actions(self,traffic, vehicle):
         if vehicle.direction == Direction.horizontal:
             return [right, left]
@@ -30,23 +26,46 @@ class IterativeDeepening(object):
             return [up, down]
 
     def get_neighbours(self, traffic):
-        operations = [] #list of traffices
+        operations = []
         for vehicle in traffic.vehicles:
             for index in range(1,5):
                 if vehicle.direction == Direction.horizontal:
                     if check_right(traffic, vehicle, index):
-                        operations.append(right(traffic, vehicle, index))
+                        op = right(traffic, vehicle, index)
+                        hash_value = self.hash(op)
+                        if hash_value not in self.visited:
+                            operations.append(op)
+                            self.visited.append(hash_value)
 
                     if check_left(traffic, vehicle, index):
-                        operations.append(left(traffic, vehicle, index))
+                        op = left(traffic, vehicle, index)
+                        hash_value = self.hash(op)
+                        if hash_value not in self.visited:
+                            operations.append(op)
+                            self.visited.append(hash_value)
                 else:
                     if check_up(traffic, vehicle, index):
-                        operations.append(up(traffic, vehicle, index))
+                        op = up(traffic, vehicle, index)
+                        hash_value = self.hash(op)
+                        if hash_value not in self.visited:
+                            operations.append(op)
+                            self.visited.append(hash_value)
 
                     if check_down(traffic, vehicle, index):
-                        operations.append(down(traffic, vehicle, index))
+                        op = down(traffic, vehicle, index)
+                        hash_value = self.hash(op)
+                        if hash_value not in self.visited:
+                            operations.append(op)
+                            self.visited.append(hash_value)
         return operations
             
+    def hash(self, state):
+        value = state.road.flatten()
+        h = ''
+        for v in value:
+           h += str(v) 
+        return h
+
     def dls(self, traffic, depth):
         if depth == 0:
             if is_goal(traffic):
@@ -57,6 +76,8 @@ class IterativeDeepening(object):
         any_remaining = False
         for child in self.get_neighbours(traffic):
             found, remaing = self.dls(child, depth-1)
+            if is_goal(child):
+                return traffic, True
             if found:
                 return found, True
             if remaing:
