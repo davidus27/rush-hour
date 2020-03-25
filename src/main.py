daed1bb7ccd10d97dbfb12aaf61bef1d9af063b2
davidus27 <davidisbest27@gmail.com>
2020-03-25 16:17:00 +0100

    Testing depth-first search of traffic

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
 import generating
 import search
-
    
 def main():
     cars = generating.create_vehicles(generating.read_file("configs/testing_config.txt"))
     traffic = generating.generate_cars(cars)
     
-    search.IterativeDeepening(traffic, cars)
+    algo = search.IterativeDeepening(traffic, cars)
 
     print(traffic.road)
-
+    print(algo.dfs(traffic, 2))
+    
+    
+    
+    """
     print("Testing:")
     print("{} car {} moves to the right: ".format(cars[1].color.name, 1), traffic.right(cars[1],1))
     print("{} car {} moves to the right: ".format(cars[2].color.name, 1), traffic.up(cars[2],1))
@@ -20,9 +23,9 @@ def main():
     print("{} car {} moves to the right: ".format(cars[7].color.name, 3), traffic.down(cars[7],3))
     print("{} car {} moves to the right: ".format(cars[4].color.name, 2), traffic.down(cars[4],2))
     print("{} car {} moves to the right: ".format(cars[0].color.name, 3), traffic.right(cars[0],3))
-
     
     print(traffic.road)
+    """
     
 if __name__ == "__main__":
     main()
